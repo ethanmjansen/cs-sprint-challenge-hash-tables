7995252696f103a01da7176569ade2c1291620f8
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-06-04 16:14:39 -0600

    Passed ex3_tests.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,10 +2,23 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+
+    for array in arrays:
+        for i in array:
+            if i in cache:
+                cache[i] += 1
+            else:
+                cache[i] = 1
+    
+    result = []
+
+    for k, v in cache.items():
+        if v == len(arrays):
+            result.append(k)
+
 
     return result
-
 
 if __name__ == "__main__":
     arrays = []
@@ -15,3 +28,4 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
