cdfc5608d2111dbc8a05e9b6a9ee718f6b7f8b6a
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-06-04 18:10:22 -0600

    Passed tests_ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    if length == 1:
+        return None
 
-    return None
+    elif length == 2:
+        first_num = weights[0]
+        second_num = weights[1]
+        if first_num + second_num == limit:
+            return [1, 0]
+        else:
+            return None
+        
+    else:
+        cache = {}
+        answer = []
+
+        for i in range(0, length):
+            cache[weights[i]] = i
+
+        for k, v in cache.items():
+            if limit - k in cache.keys():
+                answer.append(v)
+        return sorted(answer, reverse=True)
